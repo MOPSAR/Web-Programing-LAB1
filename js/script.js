3afd2857a2c14df477982a0ca74e0bc85cd92dd5
Александр Царёв <alex.carev1@icloud.com>
2026-03-17 22:35:49 +0300

    add products data array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("Project started");
+const products = [
+  {
+    id: 1,
+    title: "Наушники",
+    price: 3500,
+    image: "assets/images/headphones.jpg"
+  },
+  {
+    id: 2,
+    title: "Клавиатура",
+    price: 5200,
+    image: "assets/images/keyboard.jpg"
+  },
+  {
+    id: 3,
+    title: "Мышь",
+    price: 2400,
+    image: "assets/images/mouse.jpg"
+  },
+  {
+    id: 4,
+    title: "Монитор",
+    price: 18900,
+    image: "assets/images/monitor.jpg"
+  }
+];
